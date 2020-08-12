5c767dfca1ffe952a6d883d08ea09c2d9a3ab859
Young-W1 <youngwynn234@gmail.com>
2020-08-12 19:43:39 +0100

    Adding index.html

--- app/index.js
+++ app/index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const SERVER_PORT = process.env.SERVER_PORT;
+var SERVER_PORT = process.env.SERVER_PORT;
 
 app.use(express.static('public'));
 
-app.listen(SERVER_PORT, () => console.log('Server listening on port ' + SERVER_PORT));
+app.listen(SERVER_PORT, () => console.log('Server listening on port ' +SERVER_PORT));
